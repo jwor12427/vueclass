8597ffef45925e5f304c45a0cf246d04bd00fda2
jwor12427 <jwor12427@gmail.com>
2022-10-26 11:22:55 +0900

    2022.10.26

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <HeaderLayout />
+  <FooterLayout />
+</template>
+
+<script>
+import HeaderLayout from './components/layout/HeaderLayout.vue';
+import FooterLayout from './components/layout/FooterLayout.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderLayout,
+    FooterLayout
+}
+}
+</script>
+
+<style>
+
+</style>
